99bb22e0b769067e379ac76a081d4b593275e6dd
Freddie-Grogono <fg17761@bristol.ac.uk>
2021-10-22 11:24:18 +0100

    starting plotting

--- Code/Week 4/Week 4 R_Homework.R
+++ Code/Week 4/Week 4 R_Homework.R
@@ -26,5 +26,73 @@ long_spp <- full_join(wide_spp.1, wide_spp.2) %>%
                          tertiary_threat), 
                names_to = c("population", "date"),
                names_pattern = "(.*)_(.*)",
-               values_drop_na = F, 
+               values_drop_na = T, 
                values_to = "abundance")
+
+# Access library lubridate:
+library(lubridate) 
+
+# telling R how your date column is organised
+long_spp$date <- ymd(long_spp$date)
+
+# changing the date into a numberic value and making a new column for just date
+long_spp$year <- as.numeric(as.character(long_spp$date, "%Y"))
+
+
+
+
+# filter for the unique species: 
+unique_species <- unique(long_spp$species)
+unique_species
+
+
+# plotting the all specie's abundancies across all years:
+total_population_abundance_over_all_time <- ggplot(data= long_spp, aes (x = year, y = abundance)) + 
+  geom_point(aes(col = species)) + 
+  geom_line(aes(col = species)) + 
+  theme(legend.position = "none")
+
+total_population_abundance_over_all_time
+
+
+total_population_abundance_over_all_time_2 <- ggplot(data= long_spp, aes (x = year, y = abundance)) + 
+  geom_point(aes(col = species)) + 
+  geom_line(aes(col = species)) +
+  facet_wrap(. ~ primary_threat ) +
+  theme(legend.position = "none")
+
+total_population_abundance_over_all_time_2
+
+# filtering to different threats: 
+
+
+
+primary_threat_population <- long_spp%>% 
+  filter(primary_threat == "Pollution") 
+
+primary_threat_habitat_destruction <- long_spp%>% 
+  filter(primary_threat == "Habitat destruction") 
+
+primary_threat_exploitation<- long_spp%>% 
+  filter(primary_threat == "Exploitation") 
+
+primary_threat_climate_change <- long_spp%>% 
+  filter(primary_threat == "Climate change") 
+
+primary_threat_habitat_fragmentation <- long_spp%>% 
+  filter(primary_threat == "Habitat fragmentation") 
+
+primary_threat_habitat_loss <- long_spp%>% 
+  filter(primary_threat == "Habitat loss") 
+
+primary_threat_effects <- c(
+  
+  
+
+primary_threat_effect_on_abundance_over_time <- ggplot(data = long_spp, aes (x = year, y = abundance)) +
+  geom_point(aes(col = ))
+  
+total_population_abundance_over_all_time <- ggplot(data= long_spp, aes (x = year, y = abundance)) + 
+  geom_point(aes(col = s))
+
+total_population_abundance_over_all_time
