b5803282938cb073db22f507989fd2ae8b0d805e
Freddie-Grogono <fg17761@bristol.ac.uk>
2021-11-04 13:58:38 +0000

    update homework

--- Code/Week 6/Week 6 Homework R script.R
+++ Code/Week 6/Week 6 Homework R script.R
@@ -20,16 +20,17 @@ library(wbstats)
 my_indicators = c("GDP" = "NY.GDP.MKTP.CD")
 
 ##extract the GDP data for all countries
-GDP_Per_Cap_Data_Raw <- wb_data(my_indicators, 
+GDP_Raw <- wb_data(my_indicators, 
                     start_date = 2020, 
                     end_date = 2020)
+GDP_Raw
 
 # Get rid of useless columns from the data set: 
-GDP_Per_Cap_Data <- subset(GDP_Per_Cap_Data_Raw, select = c(1, 2, 3, 4, 5, 9))
-GDP_Per_Cap_Data # Have a look, Yay it worked! 
+GDP_Data <- subset(GDP_Raw, select = c(1, 2, 3, 4, 5, 9))
+GDP_Data # Have a look, Yay it worked! 
 
-#Making it a tibble
-GDP_Per_Cap_Data <- as_tibble(GDP_Per_Cap_Data)
+#Renaming row for join:
+GDP_Data_Final<- rename(GDP_Data, code = iso3c)
 
 # Adding a column to Tokyo Data containing country code:
 library(countrycode)
@@ -38,36 +39,26 @@ library(countrycode)
 Tokyo_Data$code <- countrycode(Tokyo_Data$Country, 
                                   origin = "country.name", 
                                   destination = "iso3c")
-GDP_Per_Cap_Data
 
+# Giving china the CHN
+Tokyo_Data$code <- sub("TWN", "CHN", Tokyo_Data$code)
+Tokyo_Data$code[42] <- "TWN"
 
 # Joining the data:
-GDP_And_Medals_Raw <- left_join(Tokyo_Data, 
-                         GDP_Per_Cap_Data %>% select(country, GDP),
-                         by = c("Country" = "country"))
+GDP_And_Medals_Raw <- inner_join(Tokyo_Data, GDP_Data_Final, by = "code")
+GDP_And_Medals_Raw
+
 
 # Remove all of the NA values 
-GDP_And_Medals <- na.omit(GDP_And_Medals_Raw)
-GDP_And_Medals
-
-# Also summarize the medals so that they are total points 
-# gold = 3, silver = 2, bronze = 1
-GDP_And_Medals$Total_Points <- ((GDP_And_Medals$Gold*5) + (GDP_And_Medals$Silver*3) + (GDP_And_Medals$Bronze*1))
-
-# Check Summary: 
-GDP_And_Medals
-# Seems to look okay, shame that we have had to omit so many countries. 
-
-# Now rank the countries based on their total points, in descending order 
-GDP_And_Medals$Rank<-rank(desc(GDP_And_Medals$Total_Points))
+GDP_And_Medals_Final <- na.omit(GDP_And_Medals_Raw)
+GDP_And_Medals_Final
 
 # Found a new method of ordering them based on what the olympic medals table actually means: i.e. ranked on the no. Gold then no. Silver then no.Bronze 
-GDP_And_Medals <-GDP_And_Medals[order(-GDP_And_Medals$Gold, -GDP_And_Medals$Silver, -GDP_And_Medals$Bronze),]
+GDP_And_Medals_Final <-GDP_And_Medals_Final[order(-GDP_And_Medals_Final$Gold, -GDP_And_Medals_Final$Silver, -GDP_And_Medals_Final$Bronze),]
 
-# Using the index to give the countries and ID 
-GDP_And_Medals <- tibble::rowid_to_column(GDP_And_Medals, "ID")
+# Using the index to give the countries a Rank:
+GDP_And_Medals_Final <- tibble::rowid_to_column(GDP_And_Medals, "Rank")
 
-# Visualize the relationship between GDP and position in the table: 
 #install the ggplot2 package
 install.packages("ggplot2")
 
@@ -75,39 +66,43 @@ install.packages("ggplot2")
 library(ggplot2)
 
 # Investigating an initial plot of medal ranking and GDP:
-p1 <- ggplot(GDP_And_Medals, aes(x = GDP,
-                                 y = ID)) +
+p1 <- ggplot(GDP_And_Medals_Final, aes(x = GDP,
+                                 y = Rank)) +
   geom_point() +
-  geom_line() +
   theme_bw() +
   ylab("Medal Ranking") +
   xlab("GDP") 
+p1
 
-p1 + geom_smooth(method="loess")
+# Pseudo logging the GDP and adding a geom_smooth line
+p1 + 
+  scale_x_continuous(trans="pseudo_log") + 
 
+    geom_smooth(method="loess")
 # It looks very messy 
 
 # Assessing the fit of a model:
-# Mod 1 is going to be the "gaussian" or normally distributed:
-mod1 <- glm(ID ~ GDP,
-                data = GDP_And_Medals,
+# Mod 1 is going to be the "Gaussian" or normally distributed:
+mod1 <- glm(Rank ~ GDP,
+                data = GDP_And_Medals_Final,
                 family = "gaussian")
 
 # Mod 2 is going to be the "poisson" type:
-mod2 <- glm(ID ~ GDP,
-            data = GDP_And_Medals,
+mod2 <- glm(Rank ~ GDP,
+            data = GDP_And_Medals_Final,
             family = "poisson")
 
-# Mod 3 is the gaussian with the link being log:
-mod3 <- glm(ID ~ GDP,
-            data = GDP_And_Medals,
+# Mod 3 is the Gaussian with the link being log:
+mod3 <- glm(Rank ~ GDP,
+            data = GDP_And_Medals_Final,
             family = gaussian(link="log"))
 
-# Mod 4 is the gaussian link with inverse link:
-mod4 <- glm(ID ~ GDP,
-            data = GDP_And_Medals,
+# Mod 4 is the Gaussian link with inverse link:
+mod4 <- glm(Rank ~ GDP,
+            data = GDP_And_Medals_Final,
             family = gaussian(link = "inverse"))
 
+# Comparing the models:
 AIC_mods <- AIC(mod1,
                 mod2,
                 mod3,
@@ -116,30 +111,44 @@ AIC_mods <- AIC(mod1,
 ## rank them by AIC using the order() function
 AIC_mods[order(AIC_mods$AIC),]
 
-# create column of log residual and log predicted 
-
-# go with inverse (4)
-
-GDP_And_Medals$pred_gaussian_log <- predict(mod3,
+# Now looking at (4)
+# create column of log predicted Gaussian
+GDP_And_Medals_Final$pred_gaussian_log <- predict(mod4,
                                         type = "response")
 
-GDP_And_Medals$resid_gaussian_log <- resid(mod3)
+#plotting the diagnostics plot for model 4
+plot(mod4)
+
+# creating a column forth residual Gaussian log
+GDP_And_Medals_Final$resid_gaussian_log <- resid(mod4)
+
+# plotting p1 with the geom line for the predicted Gaussian log
+
+p1
+p2 <- p1 + geom_line(aes(x = GDP,
+                   y = pred_gaussian_log),
+               col = "dodgerblue",
+               size = 1)
+p2
 
 
-p2 <- ggplot(GDP_And_Medals, aes ( x = GDP,
-                               y = ID)) +
+p1_1 + geom_segment(aes(xend = GDP,
+                        yend = pred_gaussian_log),
+                    col = "lightblue") 
+  
+p_hist <- ggplot(GDP_And_Medals, aes(x = resid_gaussian_log)) + geom_histogram(fill = "goldenrod") + theme_minimal() 
+p_hist  
+  
+p_pred_resid <- ggplot(GDP_And_Medals, aes(x = pred_gaussian_log,
+                                           y = resid_gaussian_log)) +
   geom_point() +
-  geom_line() +
-  theme_bw() +
-  ylab("Rank") +
-  xlab("GDP")
+  theme_minimal() +
+  geom_smooth()
 
-# Now add in a line of predicted values from the model: 
+p_pred_resid
 
-p2 <-  p2 + geom_line(aes(x = GDP,
-                          y = ID),
-                      col = "dodgerblue",
-                      size = 1)
+qqnorm(GDP_And_Medals$resid_gaussian_log); qqline(GDP_And_Medals$resid_gaussian_log)
 
-p2 <- p2 + ggtitle("Fitted model (gaussian with log link)")
-p2 
+
+
+
