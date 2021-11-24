0671c0e1f66b2809f66732b3017d90461323c3b5
Freddie-Grogono <fg17761@bristol.ac.uk>
2021-11-24 10:41:41 +0000

    Update Week_8_Session.R

---
+++ Code/r_work/Week_8/Week_8_Session.R
@@ -0,0 +1,50 @@
+
+# Task 1
+
+help <- function(){
+  print("Help")
+}
+
+help
+
+
+# Task 1.2
+# Create a function that given a vector random_vector and an integer will return how many times that integer appears inside the vector.
+
+random_vector <- c(8,9,9,8,5,1,8,5,6,2,3,5,9,9,8)
+
+
+
+func <- function(x, y){
+  a <- table(x)
+  a[names(a)==y]
+  return(a[names(a) == y])}
+
+print(func(random_vector, 9))
+
+
+# Task 1.4
+
+odds_even <- c(0,2,3,5,6,8,9)
+
+return_even <- function(x){
+  list <- c()
+  if(list(x %% 2) == 0
+     
+     
+     
+foreach num in x{
+  if((num %% 2) == 0)
+    list.append(num)
+  else
+}
+    
+
+library(vroom)
+
+task2_df      
+
+    
+    
+    
+    
